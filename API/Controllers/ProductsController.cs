a2dc3778dd61b746c6a9e86d4fa99a38a8711bb4
tonileo <mdtonileo@gmail.com>
2024-08-20 01:57:21 +0200

    Implemented repository pattern

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,80 @@
+using System;
+using Core.Entities;
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ProductsController(IProductsRepository repo) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IReadOnlyList<Product>>> GetProducts()
+    {
+        return Ok(await repo.GetProductsAsync());
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<Product>> GetProduct(int id)
+    {
+        var product = await repo.GetProductByIdAsync(id);
+
+        if (product == null) return NotFound();
+
+        return product;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<Product>> CreateProduct(Product product)
+    {
+        repo.AddProduct(product);
+        
+        if (await repo.SaveChangesAsync())
+        {
+            return CreatedAtAction("GetProduct", new {id = product.Id}, product);
+        }
+
+        return BadRequest("Problem creating the product");
+    }
+
+    [HttpPut("{id:int}")]
+    public async Task<ActionResult> UpdateProduct(int id, Product product)
+    {
+        if (product.Id != id || !ProductExists(id)) 
+            return BadRequest("Cannot update this product");
+
+        repo.UpdateProduct(product);
+
+        if (await repo.SaveChangesAsync())
+        {
+            return NoContent();
+        }
+
+        return BadRequest("Problem updating the product");
+    }
+
+    [HttpDelete("{id:int}")]
+    public async Task<ActionResult> DeleteProduct(int id)
+    {
+        var product = await repo.GetProductByIdAsync(id);
+
+        if (product == null) return NotFound();
+
+        repo.DeleteProduct(product);
+
+        if (await repo.SaveChangesAsync())
+        {
+            return NoContent();
+        }
+
+        return BadRequest("Problem deleting the product");
+    }
+
+    private bool ProductExists(int id)
+    {
+        return repo.ProductExists(id);
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,17 @@
+using Core.Interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<StoreContext>(opt =>
+{
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+builder.Services.AddScoped<IProductsRepository, ProductsRepository>();
 
 var app = builder.Build();
 
