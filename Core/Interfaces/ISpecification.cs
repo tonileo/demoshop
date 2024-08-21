9e8895ead19327cbc162db200311b0cc7f85b79d
tonileo <mdtonileo@gmail.com>
2024-08-22 01:24:17 +0200

    Implemented sorting, filtering, pagination and searching

--- Core/Interfaces/ISpecification.cs
+++ Core/Interfaces/ISpecification.cs
@@ -9,6 +9,10 @@ public interface ISpecification<T>
     Expression<Func<T, object>>? OrderBy { get; }
     Expression<Func<T, object>>? OrderByByDescending { get; }
     bool IsDistinct { get; }
+    int Take { get; }
+    int Skip {get;}
+    bool IsPagingEnabled {get;}
+    IQueryable<T> ApplyCriteria(IQueryable<T> query);
 }
 
 public interface ISpecification<T, TResult> : ISpecification<T>
