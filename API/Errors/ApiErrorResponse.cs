0ea3f71a355aa5c46d444961a68243077064a7f7
tonileo <mdtonileo@gmail.com>
2024-08-22 22:21:42 +0200

    Implemented error handling

---
+++ API/Errors/ApiErrorResponse.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace API.Errors;
+
+public class ApiErrorResponse(int statusCode, string mesage, string? details)
+{
+    public int StatusCode { get; set; } = statusCode;
+    public string Message { get; set; } = mesage;
+    public string? Details { get; set; } = details;
+}
--- API/Program.cs
+++ API/Program.cs
@@ -1,3 +1,4 @@
+using API.Middleware;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
@@ -12,10 +13,17 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 });
 
 builder.Services.AddScoped<IProductsRepository, ProductsRepository>();
-
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
+builder.Services.AddCors();
+
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseCors(x => x.AllowAnyHeader()
+    .AllowAnyMethod()
+    .WithOrigins("http://localhost:4200","https://localhost:4200"));
 
 app.MapControllers();
 
