c641e59beb16bda90f17667e87c7fdafc474f42b
tonileo <mdtonileo@gmail.com>
2024-09-04 10:39:24 +0200

    Implemented general aspn.net identity logic. Implemented login form. Updated header on login

--- API/Program.cs
+++ API/Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddCors();
 
 builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
 {
-    var connString = builder.Configuration.GetConnectionString("Redis") 
+    var connString = builder.Configuration.GetConnectionString("Redis")
         ?? throw new Exception("Cannot get redis connection string");
     var configuration = ConfigurationOptions.Parse(connString, true);
     return ConnectionMultiplexer.Connect(configuration);
@@ -40,7 +40,7 @@ app.UseMiddleware<ExceptionMiddleware>();
 app.UseCors(x => x.AllowAnyHeader()
     .AllowAnyMethod()
     .AllowCredentials()
-    .WithOrigins("http://localhost:4200","https://localhost:4200"));
+    .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.MapControllers();
 app.MapGroup("api").MapIdentityApi<AppUser>();
---
+++ client/src/app/core/services/account.service.ts
@@ -0,0 +1,42 @@
+import { inject, Injectable, signal } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Address, User } from '../../shared/models/user';
+import { map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  baseUrl = environment.apiUrl;
+  private http = inject(HttpClient);
+  
+  currentUser = signal<User | null>(null);
+
+  login(values: any) {
+    let params = new HttpParams();
+    params = params.append('useCookies', true);
+    return this.http.post<User>(this.baseUrl + 'login', values, {params, withCredentials: true});
+  }
+
+  register (values: any) {
+    return this.http.post(this.baseUrl + 'account/register', values)
+  }
+
+  getUserInfo() {
+    return this.http.get<User>(this.baseUrl + 'account/user-info', {withCredentials: true}).pipe(
+      map(user => {
+        this.currentUser.set(user);
+        return user;
+      })
+    )
+  }
+
+  logout() {
+    return this.http.post(this.baseUrl + 'account/logout', {}, {withCredentials: true});
+  }
+
+  updateAddress(address: Address) {
+    return this.http.post(this.baseUrl + 'account/address', address);
+  }
+}
