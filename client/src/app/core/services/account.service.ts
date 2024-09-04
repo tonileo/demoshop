c65bc354bd273ea328fb12cd18deba2d3cf773a5
tonileo <mdtonileo@gmail.com>
2024-09-04 10:44:49 +0200

    Implemented authentication interceptor that sends user cookie with every request

--- client/src/app/core/services/account.service.ts
+++ client/src/app/core/services/account.service.ts
@@ -16,7 +16,7 @@ export class AccountService {
   login(values: any) {
     let params = new HttpParams();
     params = params.append('useCookies', true);
-    return this.http.post<User>(this.baseUrl + 'login', values, {params, withCredentials: true});
+    return this.http.post<User>(this.baseUrl + 'login', values, {params});
   }
 
   register (values: any) {
@@ -24,7 +24,7 @@ export class AccountService {
   }
 
   getUserInfo() {
-    return this.http.get<User>(this.baseUrl + 'account/user-info', {withCredentials: true}).pipe(
+    return this.http.get<User>(this.baseUrl + 'account/user-info').pipe(
       map(user => {
         this.currentUser.set(user);
         return user;
@@ -33,7 +33,7 @@ export class AccountService {
   }
 
   logout() {
-    return this.http.post(this.baseUrl + 'account/logout', {}, {withCredentials: true});
+    return this.http.post(this.baseUrl + 'account/logout', {});
   }
 
   updateAddress(address: Address) {
