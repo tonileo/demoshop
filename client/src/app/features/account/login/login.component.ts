d8aa1f3afb7a7bb9901dd97dbb7ff0fce6a79fe5
tonileo <mdtonileo@gmail.com>
2024-09-04 19:17:38 +0200

    Implemented auth and empty-cart guard. Implemented component when cart is empty

--- client/src/app/features/account/login/login.component.ts
+++ client/src/app/features/account/login/login.component.ts
@@ -5,7 +5,7 @@ import { MatCard } from '@angular/material/card';
 import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInput } from '@angular/material/input';
 import { AccountService } from '../../../core/services/account.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -26,6 +26,14 @@ export class LoginComponent {
   private accountService = inject(AccountService);
   private router = inject(Router);
 
+  private activatedRoute = inject(ActivatedRoute);
+  returnUrl = '/shop';
+
+  constructor (){
+    const url = this.activatedRoute.snapshot.queryParams['returnUrl'];
+    if (url) this.returnUrl = url;
+  }
+
   loginForm = this.fb.group({
     email: [''],
     password: ['']
@@ -35,7 +43,7 @@ export class LoginComponent {
     this.accountService.login(this.loginForm.value).subscribe({
       next: () => {
         this.accountService.getUserInfo().subscribe();
-        this.router.navigateByUrl('/shop');
+        this.router.navigateByUrl(this.returnUrl);
       }
     })
   }
