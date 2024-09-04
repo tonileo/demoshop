7e7f24515482f4b843b24a491b4d71a9aa47dab3
tonileo <mdtonileo@gmail.com>
2024-09-04 15:29:16 +0200

    Implemented register on client side. Implemented reusable text input component

--- client/src/app/features/account/register/register.component.ts
+++ client/src/app/features/account/register/register.component.ts
@@ -1,12 +1,55 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatCard } from '@angular/material/card';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { AccountService } from '../../../core/services/account.service';
+import { Router } from '@angular/router';
+import { SnackbarService } from '../../../core/services/snackbar.service';
+import { JsonPipe } from '@angular/common';
+import { TestErrorComponent } from "../../test-error/test-error.component";
+import { TextInputComponent } from "../../../shared/components/text-input/text-input.component";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    ReactiveFormsModule,
+    MatCard,
+    MatFormField,
+    MatLabel,
+    MatInput,
+    MatButton,
+    JsonPipe,
+    TestErrorComponent,
+    TextInputComponent
+],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  private fb = inject(FormBuilder);
+  private aaccountService = inject(AccountService);
+  private router = inject(Router);
+  private snack = inject(SnackbarService);
 
+  validationError?: string[];
+
+  registerForm = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]]
+  })
+
+  onSubmit(){
+    this.aaccountService.register(this.registerForm.value).subscribe({
+      next: () => {
+        this.snack.success('Registration succesfull - you can now login');
+        this.router.navigateByUrl('/account/login');
+      },
+      error: errors => this.validationError = errors
+    })
+  }
 }
--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -7,6 +7,8 @@ import { BusyService } from '../../core/services/busy.service';
 import { MatProgressBar } from '@angular/material/progress-bar';
 import { CartService } from '../../core/services/cart.service';
 import { AccountService } from '../../core/services/account.service';
+import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
+import { MatDivider } from '@angular/material/divider';
 
 @Component({
   selector: 'app-header',
@@ -17,7 +19,11 @@ import { AccountService } from '../../core/services/account.service';
     MatBadge,
     RouterLink,
     RouterLinkActive,
-    MatProgressBar
+    MatProgressBar,
+    MatMenuTrigger,
+    MatMenu,
+    MatDivider,
+    MatMenuItem
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
