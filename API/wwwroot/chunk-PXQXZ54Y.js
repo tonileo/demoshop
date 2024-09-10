d78084bb0a4fd9034fada2dbe4067a60a313df35
tonileo <mdtonileo@gmail.com>
2024-09-10 17:52:35 +0200

    Implemented lazy loading to client side

---
+++ API/wwwroot/chunk-PXQXZ54Y.js
@@ -0,0 +1,1 @@
+import{b as u}from"./chunk-N65AT7QA.js";import{j as o}from"./chunk-7XK2MI2N.js";import{ka as t,r as e,x as n}from"./chunk-KSTUA5KL.js";var A=(m,a)=>{let r=t(u),c=t(o);return r.currentUser()?e(!0):r.getAuthState().pipe(n(i=>i.isAuthenticated?!0:(c.navigate(["/account/login"],{queryParams:{returnUrl:a.url}}),!1)))};export{A as a};
